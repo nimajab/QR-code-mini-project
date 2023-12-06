2e01c004866085efe292392cccd21cc4ece936e1
nimajab <147499101+nimajab@users.noreply.github.com>
2023-12-06 14:13:54 +0330

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ generatebtn.addEventListener('click', function (){
     return alert('لطفا يك متن ؤارد كن')
   }
   generatebtn.innerHTML = 'دريافت Qr code '
-  qrImage.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=$[]`
+  qrImage.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${qrvalue}`
   qrImage.addEventListener('load' , function (){
     qrCodeBox.classList.remove('hidden')
     generatebtn.innerHTML = 'توليد QR code '
